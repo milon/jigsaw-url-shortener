089b721da42e9d8cc11bf722bda125f984aa1ada
Nuruzzaman Milon <contact@milon.im>
2024-08-02 11:39:33 -0700

    Initial Commit

--- src/GenerateUrlRedirect.php
+++ src/GenerateUrlRedirect.php
@@ -11,10 +11,10 @@ class GenerateUrlRedirect
         $redirects = $jigsaw->getConfig()->urlRedirects;
 
         foreach ($redirects as $redirect) {
-            $stub = __DIR__ . DIRECTORY_SEPARATOR . 'stubs/redirect.blade.php';
+            $stub = __DIR__ . '/../stubs/redirect.blade.php';
             $renderedTemplate = app('view')->file($stub, ['url' => $redirect->url])->render();
 
-            $jigsaw->writeOutputFile($redirect->filename . DIRECTORY_SEPARATOR . 'index.html', $renderedTemplate);
+            $jigsaw->writeOutputFile($redirect->filename . '/index.html', $renderedTemplate);
         }
     }
 }
